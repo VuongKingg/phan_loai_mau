739c16febef420f17d48a93b1d3bb0a8b53d9ebf
ak47vina <vuong1009tb@gmail.com>
2025-06-28 16:40:17 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,71 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include <Servo.h>
-#include <LiquidCrystal.h>
 
-// Khai bao LCD (RS, E, D4, D5, D6, D7)
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+// LCD I2C
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// Khai bao cam bien mau TCS3200
+// TCS3200 color sensor
 #define S0 7
 #define S1 6
 #define S2 A1
 #define S3 A2
 #define sensorOut A0
 
-// Khai bao Servo
-Servo servoRed;
-Servo servoBlue;
-#define SERVO_RED_PIN 8
-#define SERVO_BLUE_PIN 9
+// Cảm biến quang tại vị trí servo1 (đỏ) và servo2 (xanh dương)
+#define IR_SENSOR_RED 2
+#define IR_SENSOR_BLUE 3
 
-// Khai bao Motor dieu khien boi L298N
-#define ENA 10 // PWM dieu chinh toc do
-#define IN1 A3
-#define IN2 A4
+// Motor L298N
+#define IN1 8
+#define IN2 9
 
-// Bien dem san pham
+// Servo
+Servo servo1;  // gạt màu đỏ
+Servo servo2;  // gạt màu xanh dương
+#define SERVO1_PIN 4
+#define SERVO2_PIN 5
+
+// Đếm sản phẩm
 int countRed = 0;
 int countGreen = 0;
 int countBlue = 0;
 
-// Toc do dong co (0-255)
-int motorSpeed = 150;
+// Biến lưu màu vật thể sau khi phân loại
+String currentColor = "";
 
 void setup() {
-  // Khoi dong LCD
-  lcd.begin(16, 2);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
   lcd.print("Khoi dong...");
+  delay(1000);
+  lcd.clear();
 
-  // Cai dat chan cam bien mau
   pinMode(S0, OUTPUT);
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
   pinMode(S3, OUTPUT);
   pinMode(sensorOut, INPUT);
 
-  // Cai dat Motor
-  pinMode(ENA, OUTPUT);
+  pinMode(IR_SENSOR_RED, INPUT);
+  pinMode(IR_SENSOR_BLUE, INPUT);
+
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
 
-  // Gan Servo
-  servoRed.attach(SERVO_RED_PIN);
-  servoBlue.attach(SERVO_BLUE_PIN);
-  servoRed.write(90);
-  servoBlue.write(90);
+  servo1.attach(SERVO1_PIN);
+  servo2.attach(SERVO2_PIN);
+  servo1.write(90);
+  servo2.write(90);
 
-  // Cam bien mau: Tan so cao
   digitalWrite(S0, HIGH);
   digitalWrite(S1, LOW);
 
   Serial.begin(9600);
 }
 
-// Ham doc gia tri mau
 int readColor(char color) {
   switch (color) {
     case 'R': digitalWrite(S2, LOW); digitalWrite(S3, LOW); break;
@@ -72,10 +76,9 @@ int readColor(char color) {
   return pulseIn(sensorOut, LOW);
 }
 
-void runConveyor(int speed) {
+void runConveyor() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  analogWrite(ENA, speed);
 }
 
 void stopConveyor() {
@@ -84,48 +87,76 @@ void stopConveyor() {
 }
 
 void loop() {
-  runConveyor(motorSpeed);
-
-  delay(1000); // Cho san pham den diem doc mau
-
   int red = readColor('R');
   int green = readColor('G');
   int blue = readColor('B');
 
-  Serial.print(" R:"); Serial.print(red);
+  Serial.print("R:"); Serial.print(red);
   Serial.print(" G:"); Serial.print(green);
   Serial.print(" B:"); Serial.println(blue);
 
-  // Phan loai mau du dua tren tan so do duoc
-  if (red < green && red < blue) {
-    // Vat mau do
-    stopConveyor();
-    servoRed.write(0);
-    delay(800);
-    servoRed.write(90);
-    countRed++;
-    delay(500);
-  } else if (blue < red && blue < green) {
-    // Vat mau xanh duong
-    stopConveyor();
-    servoBlue.write(0);
-    delay(800);
-    servoBlue.write(90);
-    countBlue++;
-    delay(500);
+  int minValue = min(red, min(green, blue));
+
+  if (minValue == red && red < green * 0.8 && red < blue * 0.8) {
+    currentColor = "red";
+    Serial.println("Phat hien: DO");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Phat hien: RED");
+  } else if (minValue == green && green < red * 0.8 && green < blue * 0.8) {
+    currentColor = "green";
+    Serial.println("Phat hien: XANH LA");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Phat hien: GREEN");
+  } else if (minValue == blue && blue < red * 0.8 && blue < green * 0.8) {
+    currentColor = "blue";
+    Serial.println("Phat hien: XANH DUONG");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Phat hien: BLUE");
   } else {
-    // Vat mau xanh la (di thang)
-    countGreen++;
+    currentColor = "unknown";
+    Serial.println("Khong xac dinh");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Unknown Color");
   }
 
-  // Cap nhat LCD
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Do:"); lcd.print(countRed);
-  lcd.print("XanhLa:"); lcd.print(countGreen);
+  runConveyor();
+
+  if (digitalRead(IR_SENSOR_RED) == LOW && currentColor == "red") {
+    stopConveyor();
+    servo1.write(0);
+    delay(500);
+    servo1.write(90);
+    countRed++;
+    currentColor = "";
+    delay(300);
+    runConveyor();
+  }
+
+  if (digitalRead(IR_SENSOR_BLUE) == LOW && currentColor == "blue") {
+    stopConveyor();
+    servo2.write(0);
+    delay(500);
+    servo2.write(90);
+    countBlue++;
+    currentColor = "";
+    delay(300);
+    runConveyor();
+  }
+
+  if (currentColor == "green" && digitalRead(IR_SENSOR_BLUE) == LOW) {
+    countGreen++;
+    currentColor = "";
+    delay(300);
+  }
 
   lcd.setCursor(0, 1);
-  lcd.print("XanhDuong:"); lcd.print(countBlue);
+  lcd.print("R:"); lcd.print(countRed);
+  lcd.print(" G: "); lcd.print(countGreen);
+  lcd.print(" B:"); lcd.print(countBlue);
 
-  delay(500);
+  delay(100);
 }
---
+++ src/main_backup.txt
@@ -0,0 +1,131 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include <LiquidCrystal.h>
+
+// Khai bao LCD (RS, E, D4, D5, D6, D7)
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+// Khai bao cam bien mau TCS3200
+#define S0 7
+#define S1 6
+#define S2 A1
+#define S3 A2
+#define sensorOut A0
+
+// Khai bao Servo
+Servo servoRed;
+Servo servoBlue;
+#define SERVO_RED_PIN 8
+#define SERVO_BLUE_PIN 9
+
+// Khai bao Motor dieu khien boi L298N
+#define ENA 10 // PWM dieu chinh toc do
+#define IN1 A3
+#define IN2 A4
+
+// Bien dem san pham
+int countRed = 0;
+int countGreen = 0;
+int countBlue = 0;
+
+// Toc do dong co (0-255)
+int motorSpeed = 150;
+
+void setup() {
+  // Khoi dong LCD
+  lcd.begin(16, 2);
+  lcd.print("Khoi dong...");
+
+  // Cai dat chan cam bien mau
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(sensorOut, INPUT);
+
+  // Cai dat Motor
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  // Gan Servo
+  servoRed.attach(SERVO_RED_PIN);
+  servoBlue.attach(SERVO_BLUE_PIN);
+  servoRed.write(90);
+  servoBlue.write(90);
+
+  // Cam bien mau: Tan so cao
+  digitalWrite(S0, HIGH);
+  digitalWrite(S1, LOW);
+
+  Serial.begin(9600);
+}
+
+// Ham doc gia tri mau
+int readColor(char color) {
+  switch (color) {
+    case 'R': digitalWrite(S2, LOW); digitalWrite(S3, LOW); break;
+    case 'G': digitalWrite(S2, HIGH); digitalWrite(S3, HIGH); break;
+    case 'B': digitalWrite(S2, LOW); digitalWrite(S3, HIGH); break;
+  }
+  delay(100);
+  return pulseIn(sensorOut, LOW);
+}
+
+void runConveyor(int speed) {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, speed);
+}
+
+void stopConveyor() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+}
+
+void loop() {
+  runConveyor(motorSpeed);
+
+  delay(1000); // Cho san pham den diem doc mau
+
+  int red = readColor('R');
+  int green = readColor('G');
+  int blue = readColor('B');
+
+  Serial.print(" R:"); Serial.print(red);
+  Serial.print(" G:"); Serial.print(green);
+  Serial.print(" B:"); Serial.println(blue);
+
+  // Phan loai mau du dua tren tan so do duoc
+  if (red < green && red < blue) {
+    // Vat mau do
+    stopConveyor();
+    servoRed.write(0);
+    delay(800);
+    servoRed.write(90);
+    countRed++;
+    delay(500);
+  } else if (blue < red && blue < green) {
+    // Vat mau xanh duong
+    stopConveyor();
+    servoBlue.write(0);
+    delay(800);
+    servoBlue.write(90);
+    countBlue++;
+    delay(500);
+  } else {
+    // Vat mau xanh la (di thang)
+    countGreen++;
+  }
+
+  // Cap nhat LCD
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Do:"); lcd.print(countRed);
+  lcd.print("XanhLa:"); lcd.print(countGreen);
+
+  lcd.setCursor(0, 1);
+  lcd.print("XanhDuong:"); lcd.print(countBlue);
+
+  delay(500);
+}
